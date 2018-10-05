50303ff2e2cbdf5792633872255266707f25ef40
Jan Kremen <jan@expan.do>
2018-10-05 09:24:50 +0200

    Creating controller factory

--- src/json-rpc/controller-factory.spec.ts
+++ src/json-rpc/controller-factory.spec.ts
@@ -1,6 +1,5 @@
 import {JsonRpcControllerFactory} from "./controller-factory";
 import * as express from 'express';
-import {promisify} from "util";
 import axios from 'axios';
 import {ok} from "assert";
 
@@ -16,7 +15,7 @@ describe('controler factory', function () {
         let instance = new TestService();
         let app = express();
         new TestServiceController(instance, app);
-        await (promisify(app.listen)as any)(4000);
+        await new Promise(resolve => app.listen(4000, resolve));
         let response = await axios.post('http://127.0.0.1:4000/TestService/greet', []);
         ok(response.data === 'hello');
     });
--- src/json-rpc/controller-factory.ts
+++ src/json-rpc/controller-factory.ts
@@ -12,11 +12,11 @@ export interface ControllerContructor<T> {
 export class JsonRpcControllerFactory {
     static create<T>(constructor: IControllable<T>, options?: IOptions): ControllerContructor<T> {
         let className = constructor.name;
-        let controllerName = className + 'Client';
+        let controllerName = className + 'Controller';
 
         let controllerConstructor = class {
             get instanceId() {
-                return (this.instance as any).id || this.instance
+                return (this.instance as any).id || className;
             }
 
             constructor(protected instance: T, protected express: Express) {
@@ -24,16 +24,18 @@ export class JsonRpcControllerFactory {
             }
 
             private attachToServer() {
-                this.express.post(`/${this.instanceId}/:method`, async (req: e.Request, res: e.Response) => {
-                    let method = req.params.method;
-                    let args = req.body;
-                    try {
-                        let result = await this.instance[method](...args);
-                        res.json(result);
-                    } catch (err) {
-                        res.status(500).json(err);
-                    }
-                });
+                this.express.post(`/${this.instanceId}/:method`,
+                    e.json(),
+                    async (req: e.Request, res: e.Response) => {
+                        let method = req.params.method;
+                        let args = req.body || [];
+                        try {
+                            let result = await this.instance[method](...args);
+                            res.json(result);
+                        } catch (err) {
+                            res.status(500).json(err);
+                        }
+                    });
             }
         };
 
